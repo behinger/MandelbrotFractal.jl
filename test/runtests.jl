522ad041dc4e58bfa802887aba28031cfda1961a
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-03-29 11:34:59 +0000

    plotTest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using MandelbrotFractal
 using Test 
+using UnicodePlots
 
 @testset "sanity checks" begin
     @test mandelbrot(complex(-1,1)) isa Complex
@@ -8,4 +9,22 @@ using Test
 
     @test abs(mandelbrot(complex(-1,0)) )<=2
     @test abs(mandelbrot(complex(-1,1)) )>2
+end
+
+@testset "plot" begin
+    
+    
+    y = -1:0.01:1
+    x = -2:0.01:0.5
+    mandelbrotMatrix = fill(0.,length(x),length(y))
+    for xi = eachindex(x)
+        for yi = eachindex(y)
+            mandelbrotMatrix[xi,yi] = abs(mandelbrot(complex(x[xi],y[yi])))
+        end
+    end
+    #---
+    heatmap(mandelbrotMatrix)
+    
+    
+
 end
