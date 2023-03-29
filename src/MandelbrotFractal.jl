e6289d06ef1979c732a56f5ea4a1d844c9dd8d03
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-03-29 10:18:57 +0000

    copy over the content

--- src/MandelbrotFractal.jl
+++ src/MandelbrotFractal.jl
@@ -1,5 +1,20 @@
 module MandelbrotFractal
 
-# Write your package code here.
+    # XXX add documentation
+    function mandelbrot(a)
+        z = 0
+        
+        for i=1:50
+            z = z^2 + a
+            if abs(z) > 2
+                break
+            end
+        end
+        return  z
+    end
+
+    export mandelbrot
 
 end
+
+# https://github.com/s-ccs/2023_JuliaWorkshop-PkgGitCI
