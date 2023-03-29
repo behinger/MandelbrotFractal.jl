ed54b37b54f3a571ee98aa20411081c2289d3636
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-03-29 10:10:04 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.32

---
+++ README.md
@@ -0,0 +1,5 @@
+# MandelbrotFractal
+
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://behinger.github.io/MandelbrotFractal.jl/stable/)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://behinger.github.io/MandelbrotFractal.jl/dev/)
+[![Build Status](https://github.com/behinger/MandelbrotFractal.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/behinger/MandelbrotFractal.jl/actions/workflows/CI.yml?query=branch%3Amain)
---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using MandelbrotFractal
+using Documenter
+
+DocMeta.setdocmeta!(MandelbrotFractal, :DocTestSetup, :(using MandelbrotFractal); recursive=true)
+
+makedocs(;
+    modules=[MandelbrotFractal],
+    authors="behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de> and contributors",
+    repo="https://github.com/behinger/MandelbrotFractal.jl/blob/{commit}{path}#{line}",
+    sitename="MandelbrotFractal.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://behinger.github.io/MandelbrotFractal.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/behinger/MandelbrotFractal.jl",
+    devbranch="main",
+)
