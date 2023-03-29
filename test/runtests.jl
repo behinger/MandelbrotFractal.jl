8952b5ad2c4b45360209b376e180044a43e8e9a8
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-03-29 10:18:33 +0000

    copied over the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using MandelbrotFractal
-using Test
+using Test 
 
-@testset "MandelbrotFractal.jl" begin
-    # Write your tests here.
+@testset "sanity checks" begin
+    @test mandelbrot(complex(-1,1)) isa Complex
+    @test_throws MethodError mandelbrot([1 2; 3 4])
+    @test mandelbrot(0) == 0
+
+    @test abs(mandelbrot(complex(-1,0)) )<=2
+    @test abs(mandelbrot(complex(-1,1)) )>2
 end
